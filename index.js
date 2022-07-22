1940889df2cf2c815feadaceb3b456a7a069ed42
+ <+>
2022-07-22 17:02:32 +0100

    commit tree 78b68a90edb765d7502f4af1e828320b2f823a41

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(MyArray) {
+    if (MyArray.find(data => data.result === "W")) {
+        let obj = MyArray.find(data => data.result === "W").year
+        return obj
+    } else {
+        return undefined;
+    }
+}
